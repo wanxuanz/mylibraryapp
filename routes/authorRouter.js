8e95fd2a4d3edc9114ce7f7fc76747f5ffd9a4a5
wanxuanz <wanxuanz@student.unimelb.edu.au>
2021-04-15 11:44:11 +1000

    initial commit

---
+++ routes/authorRouter.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+// add our router const author
+const authorRouter = express.Router()
+
+// require the author controller
+const authorController = require('../controllers/authorController.js')
+
+// handle the GET request toget all authors
+authorRouter.get('/', authorController.getAllAuthors)
+
+// handle the GET request to get one author
+authorRouter.get('/:id', authorController.getAuthorByID)
+
+// handle POST requests to add one author
+authorRouter.post('/', authorController.addAuthor)
+
+// export the router
+module.exports = authorRouter
